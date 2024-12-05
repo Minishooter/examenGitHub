e5845ba5b6e382fa4e69a874fba11db0143b9604
Alejandro <apueyoribes@gmail.com>
2024-12-05 16:10:45 +0100

    ExamenGitHub

---
+++ ExamenGitHub.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examengithub;
+
+import java.util.Scanner;
+
+public class ExamenGitHub {
+
+    public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);      
+  
+            int opcion = scanner.nextInt();
+        
+        do{
+        
+        switch (opcion){
+            
+            case 1:
+                
+            break;
+            
+            case 2:
+                
+            break;    
+            
+            case 3:
+                
+                break;
+            case 4:    
+ 
+                
+            default:
+            System.out.println("Opción no válida.Selecciona una opción del 1 al 4.");
+        }
+        
+        
+        
+        
+    }
+   
+    
+}
